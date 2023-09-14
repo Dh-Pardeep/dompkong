3791aec307fbdcf877dfa9001ac2562b47211e61
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-09-14 11:01:33 +0530

    mint section translateZ 35vh

--- src/components/MeetTheTeam.jsx
+++ src/components/MeetTheTeam.jsx
@@ -11,22 +11,24 @@ const MeetTheTeam = () => {
                 {MeetTheTeamMap.map((data) => {
                     return (
                         <div>
-                            <div className='border-[1px] border-solid border-[#FDDA60] shadow-[0px_4px_20px] shadow-[#FDDA60]'data-aos="zoom-in"data-aos-delay="1900" >
+                            <div className='border-[1px] border-solid border-[#FDDA60] shadow-[0px_4px_20px] shadow-[#FDDA60]' data-aos="zoom-in" data-aos-delay="1900" >
                             </div>
                             <div className='pt-[30px] pb-[65px] flex lg:justify-between flex-col lg:flex-row '>
-                                <div className='text-center lg:text-start'   data-aos="fade-right"data-aos-delay="2000"    >
+                                <div className='text-center lg:text-start' data-aos="fade-right" data-aos-delay="2000"    >
                                     <h2 className='text-white font-Montserrat text-[24px] font-bold'>{data.headingLeft}</h2>
                                     <p className='text-white font-Montserrat text-[20px] font-normal opacity-[0.9] pt-[6px]'>{data.ParaLeft}</p>
-                                    <img className='rounded-[20px] border-[3px] border-solid border-[#FDDA60] shadow-[0px_-3px_28px_-3px] shadow-[#FDDA60] mt-[32px] mx-auto lg:ms-0' src={data.img} alt="image" />
+                                    <div>
+                                        <img className='rounded-[20px] border-[3px] border-solid border-[#FDDA60] shadow-[0px_-3px_28px_-3px] shadow-[#FDDA60] mt-[32px] mx-auto lg:ms-0 transition-all ease-linear duration-300 cursor-pointer hover:scale-[0.98]' src={data.img} alt="image" />
+                                    </div>
                                 </div>
-                                <div className='text-center lg:text-start mt-8 lg:mt-0'data-aos="fade-left"data-aos-delay="1200">
+                                <div className='text-center lg:text-start mt-8 lg:mt-0' data-aos="fade-left" data-aos-delay="1200">
                                     <h2 className='text-white font-Montserrat text-[24px] font-bold'>{data.headingRight}</h2>
                                     <p className='text-white font-Montserrat text-[20px] font-normal opacity-[0.9] pt-[6px] pb-[35px]'>{data.ParaRight}</p>
                                     <p className='text-white font-Montserrat text-[16px] font-normal opacity-[0.9]  lg:text-start mx-auto max-w-[570px]'>{data.paragraphOne}</p>
                                     <p className='text-white font-Montserrat text-[16px] font-normal opacity-[0.9]   lg:text-start pt-[15px] mx-auto  max-w-[570px]'>{data.paragraphTwo}</p>
                                     <div className='flex justify-center lg:justify-start gap-4 mt-[25px] '>
-                                        <Link href='#'>{data.svgiconOne}</Link>
-                                        <Link href='#'>{data.svgiconTwo}</Link>
+                                        <a href='https://www.instagram.com/accounts/login/' target='_blank'>{data.svgiconOne}</a>
+                                        <a href='https://twitter.com/'target='_blank'>{data.svgiconTwo}</a>
                                     </div>
                                 </div>
                             </div>
